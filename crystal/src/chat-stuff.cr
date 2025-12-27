2b7270e5c8bf8a51a7c9c88c2ad4cabede79ca26
Emilia Kond <emilia@rymiel.space>
2025-12-28 01:35:01 +0200

    crystal fingerprint

--- crystal/src/chat-stuff.cr
+++ crystal/src/chat-stuff.cr
@@ -2,14 +2,26 @@ require "./chat-stuff/openssl"
 require "./proto/*"
 
 module Chatty
-  MAGIC = 0xea68u16
-  VERSION = 0u16
+  MAGIC   = 0xea68u16
+  VERSION =      0u16
 
   include OpenSSL
 
+  def self.fingerprint(key : PKey::RSA)
+    md5 = Digest::MD5.digest(key.x509_public)
+    flag = false
+    String.build do |str|
+      md5.each do |v|
+        str << ':' if flag
+        str << v.to_s(16, precision: 2)
+        flag = true
+      end
+    end
+  end
+
   if File.exists?("key.pem")
     key = File.open("key.pem") do |f|
-      PKey.read f
+      PKey::RSA.new f
     end
   else
     key = PKey::RSA.new(4096)
@@ -18,7 +30,7 @@ module Chatty
     end
   end
 
-  p! key.x509_public
+  puts "Using key #{fingerprint(key)}"
   msg = Message::RegisterRequest.new(username: "rymiel", public_signing_key: key.x509_public)
   io = IO::Memory.new
   msg.to_protobuf(io)
