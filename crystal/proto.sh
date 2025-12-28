f7a0835add17366323876a04358a3236675edb86
Emilia Kond <emilia@rymiel.space>
2025-12-28 03:44:11 +0200

    don't need this silly thing

--- crystal/proto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-crystal build lib/protobuf/bin/protoc-gen-crystal.cr -o ~/.local/bin/protoc-gen-crystal
-rm src/proto/*.pb.cr
-PROTOBUF_NS="Chatty::Message" STRIP_FROM_PACKAGE="chat.stuffs.proto" protoc -I ../proto/ --crystal_out src/proto/ ../proto/*.proto
